b89576ab744fe8af2c346a91f2ef0abf3a58b23f
sumanitian <sumanprasad26042001@gmail.com>
2021-08-03 11:04:36 +0530

    get the use data in cosole

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
+
 function App() {
   const [token, setToken] = useState(null);
-
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -12,7 +15,13 @@ function App() {
     const _token = hash.access_token;
 
     if(_token) {
-      setToken(_token)
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('show the user', user);
+      });
     }
 
     console.log('I have a token ', token);
