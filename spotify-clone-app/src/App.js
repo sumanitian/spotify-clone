14ea7e625c0af7e26008be5468fbdc728ee65aa4
sumanitian <sumanprasad26042001@gmail.com>
2021-08-03 09:39:16 +0530

    initialized the login page

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
-
+import Login from './Login';
 function App() {
   return (
     //BEM Convention
     <div className="app">
       <h1>Building Spotify clone</h1>
+
+    {/* spotify logo */}
+    {/* login  */}
+    <Login />
     </div>
   );
 }
---
+++ spotify-clone-app/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+function Login() {
+    return (
+        <div className="Login">
+            <h1>I am the login page</h1>
+            {/* spotify logo */}
+            {/* login with spotify button */}
+        </div>
+    )
+}
+
+export default Login
