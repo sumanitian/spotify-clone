5575fc286fb6e12fa3dc1054d6b7c7e551742063
sumanitian <sumanprasad26042001@gmail.com>
2021-08-03 10:52:14 +0530

    accessthe token and created logged in

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 function App() {
+  const [token, setToken] = useState(null);
+
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+    }
+
+    console.log('I have a token ', token);
+
+  }, []);
+
   return (
     //BEM Convention
     <div className="app">
-
-    {/* spotify logo */}
-    {/* login  */}
-    <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
--- spotify-clone-app/src/Login.js
+++ spotify-clone-app/src/Login.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Login.css';
+import { loginUrl } from './spotify';
 function Login() {
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"alt="" />
             {/* spotify logo */}
             {/* login with spotify button */}
-            <a>Login With spotify</a>
+            <a href={loginUrl}>Login With spotify</a>
         </div>
     )
 }
 
-export default Login
+export default Login;
