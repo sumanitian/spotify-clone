87a506d2b69548feb7863ce1b43bded5da6b6aa5
sumanitian <sumanprasad26042001@gmail.com>
2021-08-03 09:30:28 +0530

    set the margin in indexcss file

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    //BEM Convention
+    <div className="app">
       <h1>Building Spotify clone</h1>
     </div>
   );
